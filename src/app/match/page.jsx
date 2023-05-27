ca8ff5ed80367717f019a852aab732914413a67a
piwvisInnowise <126773178+piwvisInnowise@users.noreply.github.com>
2023-05-28 01:40:01 +0200

    fix localsote issue

--- src/app/match/page.jsx
+++ src/app/match/page.jsx
@@ -2,11 +2,17 @@
 import React from "react";
 
 export default function MatchPage() {
+
+  let walletAddress = "";
+  useEffect(() => {
+    walletAddress = JSON.parse(localStorage.getItem("accounts"))
+  })
+
   return (
     <div className="">
       <div className="">
          <h1 className="">#user</h1>
-         <h2>{JSON.parse(localStorage.getItem("accounts"))}</h2>
+         <h2>{walletAddress}</h2>
       </div>
     </div>
   );
